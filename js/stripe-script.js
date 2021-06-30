ea49e2fcc3389f3133ad9b74dbdd2f5a61de14be
Sara Parks <sara.parks1020@gmail.com>
2021-06-30 10:38:42 -0600

    Add files via upload

---
+++ js/stripe-script.js
@@ -0,0 +1,33 @@
+﻿var createCheckoutSession = function (priceId, selection) {
+  return fetch("/create-checkout-session", {
+    method: "POST",
+    credentials: "same-origin",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      priceId: priceId,
+      selection: selection,
+    }),
+  }).then(function (result) {
+    return result.json();
+  });
+};
+
+var stripe = Stripe(stripePK);
+
+document.getElementById("checkout").addEventListener("click", function (evt) {
+  var selection = document.querySelector("[name=btnradio]:checked").value;
+  var PriceId = PriceIdMonth;
+  if (selection == "semester") {
+    PriceId = PriceIdSemester;
+  }
+  createCheckoutSession(PriceId, selection).then(function (data) {
+    // Call Stripe.js method to redirect to the new Checkout page
+    stripe
+      .redirectToCheckout({
+        sessionId: data.sessionId,
+      })
+      .then(handleResult);
+  });
+});
